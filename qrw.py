244a59e3a808a5569388104972c2bf1dcae2e365
WrathfulSpatula <stranoj@gmail.com>
2024-04-28 15:43:17 -0400

    Revert "Symmetric +/- 1"
    This reverts commit 6f711884cb2798e3e88da62a2bf33e4d8627c8b9.

--- qrw.py
+++ qrw.py
@@ -12,11 +12,9 @@ def main():
     lattice_qb_count = 8
     lattice_qubits = list(range(lattice_qb_count))
     coin_qubit = lattice_qb_count
-    sign_qubit = lattice_qb_count - 1
-    sign_power = 1 << sign_qubit
+    sign_power = 1 << (lattice_qb_count - 1)
 
     sim = QrackSimulator(lattice_qb_count + 1)
-    sim.x(sign_qubit)
 
     for _ in range(sign_power):
         sim.h(coin_qubit)
@@ -25,8 +23,12 @@ def main():
         sim.mcsub(1, [coin_qubit], lattice_qubits)
         sim.x(coin_qubit)
 
-    exp_pos = sim.permutation_expectation(lattice_qubits) - sign_power
-    obs_pos = (sim.m_all() & ~(1 << coin_qubit)) - sign_power
+    exp_pos = sim.permutation_expectation(lattice_qubits)
+    if exp_pos >= sign_power:
+        exp_pos = exp_pos - 2 * sign_power
+    obs_pos = (sim.m_all() & ~(1 << coin_qubit))
+    if obs_pos >= sign_power:
+        obs_pos = obs_pos - 2 * sign_power
     print("Expected position:", exp_pos)
     print("Observed position:", obs_pos)
 
