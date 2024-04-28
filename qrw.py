672bfbb18b4b1b44c47f80076b000d0d4faaf33e
WrathfulSpatula <stranoj@gmail.com>
2024-04-28 14:15:10 -0400

    Debug two's complement

--- qrw.py
+++ qrw.py
@@ -12,14 +12,11 @@ def main():
     lattice_qb_count = 8
     lattice_qubits = list(range(lattice_qb_count))
     coin_qubit = lattice_qb_count
-    sign_qubit = coin_qubit - 1
-    sign_power = 1 << sign_qubit
-    steps = 1 << (lattice_qb_count - 1)
+    sign_power = 1 << (lattice_qb_count - 1)
 
     sim = QrackSimulator(lattice_qb_count + 1)
-    sim.x(lattice_qb_count - 1)
 
-    for _ in range(steps):
+    for _ in range(sign_power):
         sim.h(coin_qubit)
         sim.mcadd(1, [coin_qubit], lattice_qubits)
         sim.x(coin_qubit)
@@ -27,11 +24,11 @@ def main():
         sim.x(coin_qubit)
 
     exp_pos = sim.permutation_expectation(lattice_qubits)
-    if exp_pos >= sign_qubit:
-        exp_pos = sign_power - exp_pos
+    if exp_pos >= sign_power:
+        exp_pos = exp_pos - 2 * sign_power
     obs_pos = (sim.m_all() & ~(1 << coin_qubit))
-    if obs_pos >= sign_qubit:
-        obs_pos = sign_power - obs_pos
+    if obs_pos >= sign_power:
+        obs_pos = obs_pos - 2 * sign_power
     print("Expected position:", exp_pos)
     print("Observed position:", obs_pos)
 
