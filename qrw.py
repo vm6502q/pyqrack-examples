f7732d3ed1d719dbe6388714f4a6d0a974d60ba2
WrathfulSpatula <stranoj@gmail.com>
2024-04-28 13:55:02 -0400

    Refactor qrw.py

--- qrw.py
+++ qrw.py
@@ -12,10 +12,11 @@ def main():
     lattice_qb_count = 8
     lattice_qubits = list(range(lattice_qb_count))
     coin_qubit = lattice_qb_count
-    steps = 1 << lattice_qb_count
-    sim = QrackSimulator(lattice_qb_count + 1)
-
+    steps = 1 << (lattice_qb_count - 1)
     limit = steps - 1
+
+    sim = QrackSimulator(lattice_qb_count + 1)
+    sim.x(lattice_qb_count - 1)
 
     for _ in range(steps):
         sim.h(coin_qubit)
@@ -24,10 +25,10 @@ def main():
         sim.mcsub(1, [coin_qubit], lattice_qubits)
         sim.x(coin_qubit)
 
-    exp_pos = sim.permutation_expectation(lattice_qubits)
-    obs_pos = sim.m_all() & ~(1 << coin_qubit)
-    print("Expected position:", limit - exp_pos if exp_pos > limit else exp_pos)
-    print("Observed position:", limit - obs_pos if obs_pos > limit else obs_pos)
+    exp_pos = sim.permutation_expectation(lattice_qubits) - limit
+    obs_pos = (sim.m_all() & ~(1 << coin_qubit)) - limit
+    print("Expected position:", exp_pos)
+    print("Observed position:", obs_pos)
 
 
 if __name__ == '__main__':
