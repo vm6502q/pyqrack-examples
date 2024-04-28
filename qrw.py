4b399db2ba1e90157d35473294515d63ded04061
WrathfulSpatula <stranoj@gmail.com>
2024-04-28 14:05:24 -0400

    Proper two's complement in qrw.py

--- qrw.py
+++ qrw.py
@@ -12,8 +12,9 @@ def main():
     lattice_qb_count = 8
     lattice_qubits = list(range(lattice_qb_count))
     coin_qubit = lattice_qb_count
+    sign_qubit = coin_qubit - 1
+    sign_power = 1 << sign_qubit
     steps = 1 << (lattice_qb_count - 1)
-    limit = steps - 1
 
     sim = QrackSimulator(lattice_qb_count + 1)
     sim.x(lattice_qb_count - 1)
@@ -25,8 +26,12 @@ def main():
         sim.mcsub(1, [coin_qubit], lattice_qubits)
         sim.x(coin_qubit)
 
-    exp_pos = sim.permutation_expectation(lattice_qubits) - limit
-    obs_pos = (sim.m_all() & ~(1 << coin_qubit)) - limit
+    exp_pos = sim.permutation_expectation(lattice_qubits)
+    if exp_pos >= sign_qubit:
+        exp_pos = sign_power - exp_pos
+    obs_pos = (sim.m_all() & ~(1 << coin_qubit))
+    if obs_pos >= sign_qubit:
+        obs_pos = sign_power - obs_pos
     print("Expected position:", exp_pos)
     print("Observed position:", obs_pos)
 
