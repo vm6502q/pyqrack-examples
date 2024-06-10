2ad3bdb5eeace6b5bc0184e271419f51c5058343
WrathfulSpatula <stranoj@gmail.com>
2024-06-10 09:39:58 -0400

    Paperspace benchmarks

--- marp_2d.sh
+++ marp_2d.sh
@@ -1,1 +1,1 @@
-for depth in {1..12}; do for run in {1..100}; do python3 marp_2d.py 54 ${depth} 21 | grep "{'width':"; done; done
+for qubits in {28..33}; do export QRACK_MAX_PAGING_QB=${qubits}; echo QRACK_MAX_PAGING_QB=${qubits}; for depth in {1..12}; do for run in {1..100}; do python3 marp_2d.py 54 ${depth} 21 | grep "{'width':"; done; done; done
