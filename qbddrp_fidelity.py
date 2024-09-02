3e9b90794ca1cc8d98f414cbc9cfec19514d3788
WrathfulSpatula <stranoj@gmail.com>
2024-09-02 09:18:41 -0400

    qbddrp_fidelity.py

---
+++ qbddrp_fidelity.py
@@ -0,0 +1,163 @@
+# Demonstrates the use of "Quantum Binary Decision Diagram (QBDD) rounding parameter" ("QBDDRP")
+
+import math
+import os
+import random
+import sys
+import time
+
+import numpy as np
+
+from pyqrack import QrackSimulator
+
+
+def cx(sim, q1, q2):
+    sim.mcx([q1], q2)
+
+
+def cy(sim, q1, q2):
+    sim.mcy([q1], q2)
+
+
+def cz(sim, q1, q2):
+    sim.mcz([q1], q2)
+
+
+def acx(sim, q1, q2):
+    sim.macx([q1], q2)
+
+
+def acy(sim, q1, q2):
+    sim.macy([q1], q2)
+
+
+def acz(sim, q1, q2):
+    sim.macz([q1], q2)
+
+
+def swap(sim, q1, q2):
+    sim.swap(q1, q2)
+
+
+def iswap(sim, q1, q2):
+    sim.iswap(q1, q2)
+
+
+def iiswap(sim, q1, q2):
+    sim.adjiswap(q1, q2)
+
+
+def pswap(sim, q1, q2):
+    sim.mcz([q1], q2)
+    sim.swap(q1, q2)
+
+
+def mswap(sim, q1, q2):
+    sim.swap(q1, q2)
+    sim.mcz([q1], q2)
+
+
+def nswap(sim, q1, q2):
+    sim.mcz([q1], q2)
+    sim.swap(q1, q2)
+    sim.mcz([q1], q2)
+
+
+def bench_qrack(width, depth):
+    # This is a "nearest-neighbor" coupler random circuit.
+    experiment = QrackSimulator(width, isBinaryDecisionTree=True)
+    control = QrackSimulator(width)
+    control.set_sdrp(5.9604645e-8)
+
+    lcv_range = range(width)
+    all_bits = list(lcv_range)
+
+    # Nearest-neighbor couplers:
+    gateSequence = [ 0, 3, 2, 1, 2, 1, 0, 3 ]
+    two_bit_gates = swap, pswap, mswap, nswap, iswap, iiswap, cx, cy, cz, acx, acy, acz
+
+    col_len = math.floor(math.sqrt(width))
+    while (((width // col_len) * col_len) != width):
+        col_len -= 1
+    row_len = width // col_len
+
+    f = []
+    t = []
+    t_tot = 0
+
+    for _ in range(depth):
+        start = time.perf_counter()
+        # Single-qubit gates
+        for i in lcv_range:
+            th = random.uniform(0, 2 * math.pi)
+            ph = random.uniform(0, 2 * math.pi)
+            lm = random.uniform(0, 2 * math.pi)
+            experiment.u(i, th, ph, lm)
+            control.u(i, th, ph, lm)
+
+        # Nearest-neighbor couplers:
+        ############################
+        gate = gateSequence.pop(0)
+        gateSequence.append(gate)
+        for row in range(1, row_len, 2):
+            for col in range(col_len):
+                temp_row = row
+                temp_col = col
+                temp_row = temp_row + (1 if (gate & 2) else -1);
+                temp_col = temp_col + (1 if (gate & 1) else 0)
+
+                if (temp_row < 0) or (temp_col < 0) or (temp_row >= row_len) or (temp_col >= row_len):
+                    continue
+
+                b1 = row * row_len + col
+                b2 = temp_row * row_len + temp_col
+
+                if (b1 >= width) or (b2 >= width):
+                    continue
+
+                g = random.choice(two_bit_gates)
+                g(experiment, b1, b2)
+                g(control, b1, b2)
+
+        _t = time.perf_counter() - start
+        t_tot = t_tot + _t
+        t.append(t_tot)
+
+        experiment_sv = experiment.out_ket()
+        control_sv = control.out_ket()
+    
+        f.append(np.abs(sum([np.conj(x) * y for x, y in zip(experiment_sv, control_sv)])))
+
+    return (t, f)
+
+
+def main():
+    if len(sys.argv) < 4:
+        raise RuntimeError('Usage: python3 sdrp.py [qbddrp] [sdrp] [samples]')
+
+    qbddrp = float(sys.argv[1])
+    if (qbddrp > 0):
+        os.environ['QRACK_QBDT_SEPARABILITY_THRESHOLD'] = sys.argv[1]
+
+    sdrp = float(sys.argv[2])
+    if (sdrp > 0):
+        os.environ['QRACK_QUNIT_SEPARABILITY_THRESHOLD'] = sys.argv[2]
+
+    samples = int(sys.argv[3])
+
+    # Run the benchmarks
+    width_results = []
+    for i in [4, 9, 16]:
+        width_results.append([])
+        for j in range(samples):
+            width_results[-1].append(bench_qrack(i, i))
+
+    # time_result = sum(t for t in width_results) / samples
+    # print("Width=" + str(width) + ", Depth=" + str(depth) + ": " + str(time_result) + " seconds. (Fidelity is unknown.)")
+    print(width_results)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
