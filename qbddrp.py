ee74c7f2cb21603338560626f277446d0b925b53
WrathfulSpatula <stranoj@gmail.com>
2024-09-04 17:17:22 -0400

    Restore nearest-neighbor QBDDRP

--- qbddrp.py
+++ qbddrp.py
@@ -9,6 +9,58 @@ import time
 from pyqrack import QrackSimulator
 
 
+def cx(sim, q1, q2):
+    sim.mcx([q1], q2)
+
+
+def cy(sim, q1, q2):
+    sim.mcy([q1], q2)
+
+
+def cz(sim, q1, q2):
+    sim.mcz([q1], q2)
+
+
+def acx(sim, q1, q2):
+    sim.macx([q1], q2)
+
+
+def acy(sim, q1, q2):
+    sim.macy([q1], q2)
+
+
+def acz(sim, q1, q2):
+    sim.macz([q1], q2)
+
+
+def swap(sim, q1, q2):
+    sim.swap(q1, q2)
+
+
+def iswap(sim, q1, q2):
+    sim.iswap(q1, q2)
+
+
+def iiswap(sim, q1, q2):
+    sim.adjiswap(q1, q2)
+
+
+def pswap(sim, q1, q2):
+    sim.mcz([q1], q2)
+    sim.swap(q1, q2)
+
+
+def mswap(sim, q1, q2):
+    sim.swap(q1, q2)
+    sim.mcz([q1], q2)
+
+
+def nswap(sim, q1, q2):
+    sim.mcz([q1], q2)
+    sim.swap(q1, q2)
+    sim.mcz([q1], q2)
+
+
 def bench_qrack(width, depth):
     # This is a "nearest-neighbor" coupler random circuit.
     start = time.perf_counter()
@@ -18,20 +70,44 @@ def bench_qrack(width, depth):
     lcv_range = range(width)
     all_bits = list(lcv_range)
 
+    # Nearest-neighbor couplers:
+    gateSequence = [ 0, 3, 2, 1, 2, 1, 0, 3 ]
+    two_bit_gates = swap, pswap, mswap, nswap, iswap, iiswap, cx, cy, cz, acx, acy, acz
+
+    col_len = math.floor(math.sqrt(width))
+    while (((width // col_len) * col_len) != width):
+        col_len -= 1
+    row_len = width // col_len
+    if col_len == 1:
+        raise Exception("ERROR: Can't simulate prime number width!")
+
     for _ in range(depth):
         # Single-qubit gates
         for i in lcv_range:
             sim.u(i, random.uniform(0, 2 * math.pi), random.uniform(0, 2 * math.pi), random.uniform(0, 2 * math.pi))
 
-        # 2-qubit couplers
-        unused_bits = all_bits.copy()
-        random.shuffle(unused_bits)
-        while len(unused_bits) > 1:
-            c = unused_bits.pop()
-            t = unused_bits.pop()
-            sim.h(t)
-            sim.mcz([c], t)
-            sim.h(t)
+        # Nearest-neighbor couplers:
+        ############################
+        gate = gateSequence.pop(0)
+        gateSequence.append(gate)
+        for row in range(1, row_len, 2):
+            for col in range(col_len):
+                temp_row = row
+                temp_col = col
+                temp_row = temp_row + (1 if (gate & 2) else -1);
+                temp_col = temp_col + (1 if (gate & 1) else 0)
+
+                if (temp_row < 0) or (temp_col < 0) or (temp_row >= row_len) or (temp_col >= row_len):
+                    continue
+
+                b1 = row * row_len + col
+                b2 = temp_row * row_len + temp_col
+
+                if (b1 >= width) or (b2 >= width):
+                    continue
+
+                g = random.choice(two_bit_gates)
+                g(sim, b1, b2)
 
     # Terminal measurement
     sim.m_all()
