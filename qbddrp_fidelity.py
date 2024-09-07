896b1dcd431d5d0efba6f5f7bf59f9df0c0929df
WrathfulSpatula <stranoj@gmail.com>
2024-09-07 10:40:08 -0400

    Per-gate fidelity

--- qbddrp_fidelity.py
+++ qbddrp_fidelity.py
@@ -14,54 +14,66 @@ from pyqrack import QrackSimulator
 
 def cx(sim, q1, q2):
     sim.mcx([q1], q2)
+    return 1
 
 
 def cy(sim, q1, q2):
     sim.mcy([q1], q2)
+    return 1
 
 
 def cz(sim, q1, q2):
     sim.mcz([q1], q2)
+    return 1
 
 
 def acx(sim, q1, q2):
     sim.macx([q1], q2)
+    return 1
 
 
 def acy(sim, q1, q2):
     sim.macy([q1], q2)
+    return 1
 
 
 def acz(sim, q1, q2):
     sim.macz([q1], q2)
+    return 1
 
 
 def swap(sim, q1, q2):
     sim.swap(q1, q2)
+    return 1
 
 
 def iswap(sim, q1, q2):
     sim.iswap(q1, q2)
+    return 1
 
 
 def iiswap(sim, q1, q2):
     sim.adjiswap(q1, q2)
+    return 1
 
 
 def pswap(sim, q1, q2):
     sim.mcz([q1], q2)
     sim.swap(q1, q2)
+    return 2
 
 
 def mswap(sim, q1, q2):
     sim.swap(q1, q2)
     sim.mcz([q1], q2)
+    return 2
 
 
 def nswap(sim, q1, q2):
     sim.mcz([q1], q2)
     sim.swap(q1, q2)
     sim.mcz([q1], q2)
+    return 3
 
 
 def bench_qrack(width, depth):
@@ -84,6 +96,8 @@ def bench_qrack(width, depth):
         print("(Prime - skipped)")
         return
 
+    gate_count = 0
+
     for d in range(depth):
         start = time.perf_counter()
         # Single-qubit gates
@@ -93,6 +107,7 @@ def bench_qrack(width, depth):
             lm = random.uniform(0, 2 * math.pi)
             experiment.u(i, th, ph, lm)
             control.u(i, th, ph, lm)
+            gate_count = gate_count + 1
 
         # Nearest-neighbor couplers:
         ############################
@@ -116,12 +131,15 @@ def bench_qrack(width, depth):
 
                 g = random.choice(two_bit_gates)
                 g(experiment, b1, b2)
-                g(control, b1, b2)
+                gate_count = gate_count + g(control, b1, b2)
 
         experiment_sv = experiment.out_ket()
         control_sv = control.out_ket()
 
-        print("Depth=" + str(d + 1) + ", fidelity=" + str(np.abs(sum([np.conj(x) * y for x, y in zip(experiment_sv, control_sv)]))))
+        overall_fidelity = np.abs(sum([np.conj(x) * y for x, y in zip(experiment_sv, control_sv)]))
+        per_gate_fidelity = overall_fidelity ** (1 / gate_count)
+
+        print("Depth=" + str(d + 1) + ", overall fidelity=" + str(overall_fidelity) + ", per-gate fidelity avg.=" + str(per_gate_fidelity))
 
 
 def main():
