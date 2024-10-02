cd23e86d38ab1d86799285a95bb99ba24f39230d
WrathfulSpatula <stranoj@gmail.com>
2024-10-02 10:24:38 -0400

    Near-Clifford fully-connected

---
+++ qbdd_nc_fc.py
@@ -0,0 +1,63 @@
+# Demonstrates the use of "Quantum Binary Decision Diagram (QBDD) rounding parameter" ("QBDDRP")
+
+import math
+import os
+import random
+import sys
+import time
+
+from pyqrack import QrackSimulator
+
+
+def bench_qrack(width, depth):
+    # This is a "nearest-neighbor" coupler random circuit.
+    start = time.perf_counter()
+
+    sim = QrackSimulator(width, isBinaryDecisionTree=True)
+
+    lcv_range = range(width)
+    all_bits = list(lcv_range)
+
+    for _ in range(depth):
+        # Single-qubit gates
+        for i in lcv_range:
+            for _ in range(3):
+                # x-z-x Euler axes
+                sim.h(i)
+                sim.r(Pauli.PauliZ, random.uniform(0, 2 * math.pi), i)
+            sim.h(i)
+
+        # 2-qubit couplers
+        unused_bits = all_bits.copy()
+        random.shuffle(unused_bits)
+        while len(unused_bits) > 1:
+            c = unused_bits.pop()
+            t = unused_bits.pop()
+            sim.h(t)
+            sim.mcz([c], t)
+            sim.h(t)
+
+    # Terminal measurement
+    sim.m_all()
+
+    return time.perf_counter() - start
+
+
+def main():
+    if len(sys.argv) < 3:
+        raise RuntimeError('Usage: python3 qbdd_fc.py [width] [depth]')
+
+    width = int(sys.argv[1])
+
+    depth = int(sys.argv[2])
+
+    # Run the benchmarks
+    time_result = bench_qrack(width, depth)
+
+    print("Width=" + str(width) + ", Depth=" + str(depth) + ": " + str(time_result) + " seconds. (Fidelity is unknown.)")
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ qbdd_nc_fc_fidelity_qiskit.py
@@ -0,0 +1,97 @@
+# Demonstrates the use of "Quantum Binary Decision Diagram (QBDD) rounding parameter" ("QBDDRP")
+
+import math
+import os
+import random
+import sys
+import time
+
+import numpy as np
+
+from pyqrack import QrackSimulator
+
+from qiskit import QuantumCircuit
+from qiskit.compiler import transpile
+from qiskit_aer.backends import AerSimulator
+
+
+def rand_u3(sim, q):
+    sim.h(q)
+    sim.rz(random.uniform(0, 2 * math.pi), q)
+    sim.h(q)
+    sim.rz(random.uniform(0, 2 * math.pi), q)
+    sim.h(q)
+    sim.rz(random.uniform(0, 2 * math.pi), q)
+    sim.h(q)
+
+
+def coupler(sim, q1, q2):
+    sim.h(q2)
+    sim.cz(q1, q2)
+    sim.h(q2)
+
+
+def bench_qrack(width, depth):
+    # This is a "nearest-neighbor" coupler random circuit.
+    circ = QuantumCircuit(width)
+    experiment = QrackSimulator(width, isBinaryDecisionTree=True)
+    control = AerSimulator(method="statevector")
+
+    lcv_range = range(width)
+    all_bits = list(lcv_range)
+
+    # Nearest-neighbor couplers:
+    gateSequence = [ 0, 3, 2, 1, 2, 1, 0, 3 ]
+    two_bit_gates = swap, pswap, mswap, nswap, iswap, iiswap, cx, cy, cz, acx, acy, acz
+
+    col_len = math.floor(math.sqrt(width))
+    while (((width // col_len) * col_len) != width):
+        col_len -= 1
+    row_len = width // col_len
+    if col_len == 1:
+        print("(Prime - skipped)")
+        return
+
+    for d in range(depth):
+        experiment.reset_all()
+        start = time.perf_counter()
+        # Single-qubit gates
+        for i in lcv_range:
+            rand_u3(circ, i)
+
+        # 2-qubit couplers
+        unused_bits = all_bits.copy()
+        random.shuffle(unused_bits)
+        while len(unused_bits) > 1:
+            c = unused_bits.pop()
+            t = unused_bits.pop()
+            coupler(circ, c, t)
+
+        gate_count = sum(dict(circ.count_ops()).values())
+
+        experiment.run_qiskit_circuit(circ)
+
+        circ_aer = transpile(circ, backend=control)
+        circ_aer.save_statevector()
+        job = control.run(circ_aer)
+
+        experiment_sv = experiment.out_ket()
+        control_sv = np.asarray(job.result().get_statevector())
+
+        overall_fidelity = np.abs(sum([np.conj(x) * y for x, y in zip(experiment_sv, control_sv)]))
+        per_gate_fidelity = overall_fidelity ** (1 / gate_count)
+
+        print("Depth=" + str(d + 1) + ", overall fidelity=" + str(overall_fidelity) + ", per-gate fidelity avg.=" + str(per_gate_fidelity))
+
+
+def main():
+    # Run the benchmarks
+    for i in range(1, 21):
+        print("Width=" + str(i) + ":")
+        bench_qrack(i, i)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
