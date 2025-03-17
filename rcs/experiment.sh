2c9f67beaca191d3187de411fc020836a93c76b2
WrathfulSpatula <stranoj@gmail.com>
2025-03-17 12:52:23 -0400

    Revert experiment.sh

--- rcs/experiment.sh
+++ rcs/experiment.sh
@@ -1,12 +1,1 @@
-echo "sycamore_2019_elided"
-for run in {1..10}; do python3 sycamore_2019_elided.py 28 10; done
-echo "rcs_nn_elided"
-for run in {1..10}; do python3 rcs_nn_elided.py 28 10; done
-echo "fc_elided"
-for run in {1..10}; do python3 fc_elided.py 28 10; done
-echo "sycamore_2019_elided_time"
-for run in {1..10}; do python3 sycamore_2019_elided_time.py 54 20; done
-echo "rcs_nn_elided_time"
-for run in {1..10}; do python3 rcs_nn_elided_time.py 54 20; done
-echo "fc_elided_time"
-for run in {1..10}; do python3 fc_elided_time.py 54 20; done
+for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25 26 27 28 ; do QRACK_MAX_PAGING_QB=$(($w / 2)) QRACK_MAX_CPU_QB=$(($w / 2)) python3 rcs_nn_qiskit_validation.py $w $w; done
