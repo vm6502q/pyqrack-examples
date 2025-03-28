fcabfae610e13da7a90cbf3722fca74bf97ae0d8
WrathfulSpatula <stranoj@gmail.com>
2025-03-27 21:19:02 -0400

    Fully-connected experiment.sh

--- rcs/experiment.sh
+++ rcs/experiment.sh
@@ -1,1 +1,1 @@
-for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25 26 27 28 ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 rcs_nn_qiskit_validation.py $w $w; done
+for w in {2..28} ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 fc_qiskit_validation.py $w $w 20; done
