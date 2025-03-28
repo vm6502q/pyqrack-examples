d3eab2ab699634612de781786ddef29899eb6412
WrathfulSpatula <stranoj@gmail.com>
2025-03-27 21:36:36 -0400

    30 trials in experiment.sh

--- rcs/experiment.sh
+++ rcs/experiment.sh
@@ -1,1 +1,1 @@
-for w in {2..28} ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 fc_qiskit_validation.py $w $w 20; done
+for w in {2..28} ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 fc_qiskit_validation.py $w $w 30; done
