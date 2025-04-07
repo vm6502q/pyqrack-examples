0c6c94543f28812cad8055e8a995bda6c29dccde
WrathfulSpatula <stranoj@gmail.com>
2025-04-07 13:24:10 -0400

    10 Trotter steps in experiment.sh

--- ising/experiment.sh
+++ ising/experiment.sh
@@ -1,1 +1,1 @@
-for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25 26 27 28 ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 ising_ace_validation.py 5 $w; done
+for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25 26 27 28 ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 ising_ace_validation.py 10 $w; done
