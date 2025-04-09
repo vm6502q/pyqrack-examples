11d53f27436054915d8b03cb97942651a3402bae
WrathfulSpatula <stranoj@gmail.com>
2025-04-09 18:05:28 -0400

    2048 Hamming distance window in experiment.sh

--- ising/experiment.sh
+++ ising/experiment.sh
@@ -1,1 +1,1 @@
-for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 ising_ace_validation.py 10 $w; done
+for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 ising_ace_validation.py 10 $w 2048; done
