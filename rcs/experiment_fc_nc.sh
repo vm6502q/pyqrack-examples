24d0dbbda5131e0d0ba863924dee5c7b3e988bd8
WrathfulSpatula <stranoj@gmail.com>
2025-04-19 17:54:07 -0400

    SDRP=0.025

--- rcs/experiment_fc_nc.sh
+++ rcs/experiment_fc_nc.sh
@@ -1,4 +1,4 @@
 # Suggested value for SDRP:
-export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.02
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.025
 
 for w in {2..50} ; do QRACK_QTENSORNETWORK_THRESHOLD_QB=49 python3 fc_nc.py $w $w; done
