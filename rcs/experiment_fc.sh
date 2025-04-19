f23a12eb8bc5ab7e2fd57b2bcdbb613405cd3f81
WrathfulSpatula <stranoj@gmail.com>
2025-04-19 18:43:45 -0400

    Error-margin SDRP tuning

--- rcs/experiment_fc.sh
+++ rcs/experiment_fc.sh
@@ -1,4 +1,4 @@
 # Suggested value for SDRP:
-export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.024
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.02405
 
 for w in {2..28} ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 fc_qiskit_validation.py $w $w 30; done
