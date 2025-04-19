825a068ac41b5cebe07dfd7cd7554d5bf2988302
WrathfulSpatula <stranoj@gmail.com>
2025-04-19 14:29:30 -0400

    Fix suggested SDRP

--- ising/experiment.sh
+++ ising/experiment.sh
@@ -1,4 +1,4 @@
 # Suggested value for SDRP:
-export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.2
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.03
 
 for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 ising_ace_validation.py 10 $w 2048; done
