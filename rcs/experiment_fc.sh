c2a4c6a6f5a20403ab2492552d9e4f9e3ed07113
WrathfulSpatula <stranoj@gmail.com>
2025-05-11 22:54:35 -0400

    SDRP=0.02375

--- rcs/experiment_fc.sh
+++ rcs/experiment_fc.sh
@@ -1,6 +1,6 @@
 # Turn off light-cone optimization
 export QRACK_QTENSORNETWORK_THRESHOLD_QB=-1
 # Suggested value for SDRP
-export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.024
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.02375
 
 for w in {2..28} ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 fc_qiskit_validation.py $w $w 30; done
--- rcs/experiment_fc_nc.sh
+++ rcs/experiment_fc_nc.sh
@@ -1,6 +1,6 @@
 # Turn off light-cone optimization
 export QRACK_QTENSORNETWORK_THRESHOLD_QB=-1
 # Suggested value for SDRP
-export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.024
+export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.02375
 
 for w in {2..50} ; do QRACK_QTENSORNETWORK_THRESHOLD_QB=49 python3 fc_nc.py $w $w; done
