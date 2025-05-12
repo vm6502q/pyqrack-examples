f04fd29c29d69af995aa337b517221c23e9f913c
WrathfulSpatula <stranoj@gmail.com>
2025-05-12 17:19:59 -0400

    Don't change QRACK_MAX_PAGE_QB

--- nc/experiment_fc.sh
+++ nc/experiment_fc.sh
@@ -1,6 +1,5 @@
 # Turn off near-Clifford conversion to state vector
 export QRACK_MAX_PAGING_QB=-1
-export QRACK_MAX_PAGE_QB=-1
 export QRACK_MAX_CPU_QB=-1
 
 for w in {2..28} ; do python3 fc_qiskit_validation.py $w $w 30; done
--- nc/experiment_nn.sh
+++ nc/experiment_nn.sh
@@ -1,6 +1,5 @@
 # Turn off near-Clifford conversion to state vector
 export QRACK_MAX_PAGING_QB=-1
-export QRACK_MAX_PAGE_QB=-1
 export QRACK_MAX_CPU_QB=-1
 
 for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25 26 27 28 ; do python3 rcs_nn_qiskit_validation.py $w $w; done
