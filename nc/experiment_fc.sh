4a5708d72cd4210ed77e074f6da5d4eacd4904d9
WrathfulSpatula <stranoj@gmail.com>
2025-05-12 15:14:02 -0400

    Fix fully-connected experiment

--- nc/experiment_fc.sh
+++ nc/experiment_fc.sh
@@ -5,4 +5,4 @@ export QRACK_MAX_PAGING_QB=-1
 export QRACK_MAX_PAGE_QB=-1
 export QRACK_MAX_CPU_QB=-1
 
-for w in {2..28} ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 fc_qiskit_validation.py $w $w 30; done
+for w in {2..28} ; do python3 fc_qiskit_validation.py $w $w 30; done
