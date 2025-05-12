ad01a668e180cd3528621d4220d9ddce1a91ed5f
WrathfulSpatula <stranoj@gmail.com>
2025-05-12 14:21:50 -0400

    Near-Clifford

---
+++ nc/experiment_fc.sh
@@ -0,0 +1,10 @@
+# Turn off SDRP
+unset QRACK_QUNIT_SEPARABILITY_THRESHOLD
+# Turn off near-Clifford conversion to state vector
+export QRACK_MAX_PAGING_QB=-1
+export QRACK_MAX_PAGE_QB=-1
+export QRACK_MAX_CPU_QB=-1
+# Round to nearest Clifford circuit
+export QRACK_NONCLIFFORD_ROUNDING_THRESHOLD=1
+
+for w in {2..28} ; do QRACK_MAX_PAGING_QB=$((($w + 1) / 2)) QRACK_MAX_CPU_QB=$((($w + 1) / 2)) python3 fc_qiskit_validation.py $w $w 30; done
