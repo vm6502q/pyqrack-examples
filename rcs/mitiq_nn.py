597b5bf0903fb58e7d625e7955084e6f7c0c053c
WrathfulSpatula <stranoj@gmail.com>
2025-05-16 18:58:14 -0400

    mitiq_nn.py

---
+++ rcs/mitiq_nn.py
@@ -0,0 +1,214 @@
+# See "Error mitigation increases the effective quantum volume of quantum computers," https://arxiv.org/abs/2203.05489
+#
+# Mitiq is under the GPL 3.0.
+# Hence, this example, as the entire work-in-itself, must be considered to be under GPL 3.0.
+# See https://www.gnu.org/licenses/gpl-3.0.txt for details.
+
+import math
+import random
+import statistics
+import sys
+import time
+
+import numpy as np
+
+from collections import Counter
+
+from pyqrack import QrackSimulator
+
+from qiskit import QuantumCircuit
+from qiskit.compiler import transpile
+from qiskit_aer.backends import AerSimulator
+from qiskit.quantum_info import Statevector
+
+from mitiq import zne
+from mitiq.zne.scaling.folding import fold_global
+from mitiq.zne.inference import LinearFactory
+
+    
+def factor_width(width):
+    col_len = math.floor(math.sqrt(width))
+    while (((width // col_len) * col_len) != width):
+        col_len -= 1
+    row_len = width // col_len
+    if col_len == 1:
+        raise Exception("ERROR: Can't simulate prime number width!")
+
+    return (row_len, col_len)
+
+
+def cx(sim, q1, q2):
+    sim.cx(q1, q2)
+
+
+def cy(sim, q1, q2):
+    sim.cy(q1, q2)
+
+
+def cz(sim, q1, q2):
+    sim.cz(q1, q2)
+
+
+def acx(sim, q1, q2):
+    sim.x(q1)
+    sim.cx(q1, q2)
+    sim.x(q1)
+
+
+def acy(sim, q1, q2):
+    sim.x(q1)
+    sim.cy(q1, q2)
+    sim.x(q1)
+
+
+def acz(sim, q1, q2):
+    sim.x(q1)
+    sim.cz(q1, q2)
+    sim.x(q1)
+
+
+def swap(sim, q1, q2):
+    sim.swap(q1, q2)
+
+
+def iswap(sim, q1, q2):
+    sim.iswap(q1, q2)
+
+
+def iiswap(sim, q1, q2):
+    sim.iswap(q1, q2)
+    sim.iswap(q1, q2)
+    sim.iswap(q1, q2)
+
+
+def pswap(sim, q1, q2):
+    sim.cz(q1, q2)
+    sim.swap(q1, q2)
+
+
+def mswap(sim, q1, q2):
+    sim.swap(q1, q2)
+    sim.cz(q1, q2)
+
+
+def nswap(sim, q1, q2):
+    sim.cz(q1, q2)
+    sim.swap(q1, q2)
+    sim.cz(q1, q2)
+
+
+def random_circuit(width, depth):
+    # This is a "nearest-neighbor" coupler random circuit.
+    lcv_range = range(width)
+    all_bits = list(lcv_range)
+    
+    # Nearest-neighbor couplers:
+    gateSequence = [ 0, 3, 2, 1, 2, 1, 0, 3 ]
+    two_bit_gates = swap, pswap, mswap, nswap, iswap, iiswap, cx, cy, cz, acx, acy, acz
+    
+    row_len, col_len = factor_width(width)
+    
+    results = []
+
+    circ = QuantumCircuit(width)
+    for d in range(depth):
+        # Single-qubit gates
+        for i in lcv_range:
+            circ.h(i)
+            circ.rz(random.uniform(0, 2 * math.pi), i)
+
+        # Nearest-neighbor couplers:
+        ############################
+        gate = gateSequence.pop(0)
+        gateSequence.append(gate)
+        for row in range(1, row_len, 2):
+            for col in range(col_len):
+                temp_row = row
+                temp_col = col
+                temp_row = temp_row + (1 if (gate & 2) else -1);
+                temp_col = temp_col + (1 if (gate & 1) else 0)
+
+                if temp_row < 0:
+                    temp_row = temp_row + row_len
+                if temp_col < 0:
+                    temp_col = temp_col + col_len
+                if temp_row >= row_len:
+                    temp_row = temp_row - row_len
+                if temp_col >= col_len:
+                    temp_col = temp_col - col_len
+
+                b1 = row * row_len + col
+                b2 = temp_row * row_len + temp_col
+
+                if (b1 >= width) or (b2 >= width):
+                    continue
+
+                g = random.choice(two_bit_gates)
+                g(circ, b1, b2)
+
+    return circ
+
+def logit(x):
+    # Theoretically, these limit points are "infinite,"
+    # but precision caps out between 36 and 37:
+    if 5e-17 > (1 - x):
+        return 37
+    # For the negative limit, the precision caps out
+    # between -37 and -38
+    elif x < 1e-17:
+        return -38
+    return max(-38, min(37, np.log(x / (1 - x))))
+
+
+def expit(x):
+    # Theoretically, these limit points are "infinite,"
+    # but precision caps out between 36 and 37:
+    if x >= 37:
+        return 1.0
+    # For the negative limit, the precision caps out
+    # between -37 and -38
+    elif x <= -38:
+        return 0.0
+    return 1 / (1 + np.exp(-x))
+
+
+def execute(circ):
+    """Returns the mirror circuit expectation value for unsigned integer overall bit string."""
+
+    all_bits = list(range(circ.width()))
+    
+    experiment = QrackSimulator(circ.width())
+    experiment.run_qiskit_circuit(circ)
+
+    # We might be surprised if Haar-random magnetization is nontrivial.
+    magnetization = 0
+    for qubit in all_bits:
+        z_exp = 1 - 2 * experiment.prob(qubit)
+        magnetization += z_exp
+    magnetization /= circ.width()
+
+    return logit((magnetization + 1) / 2)
+
+
+def main():
+    if len(sys.argv) < 3:
+        raise RuntimeError('Usage: python3 fc.py [width] [depth]')
+
+    width = int(sys.argv[1])
+    depth = int(sys.argv[2])
+
+    circ = random_circuit(width, depth)
+
+    scale_count = 9
+    max_scale = 5
+    factory = LinearFactory(scale_factors=[(1 + (max_scale - 1) * x / scale_count) for x in range(0, scale_count)])
+
+    magnetiization = expit(zne.execute_with_zne(circ, execute, scale_noise=fold_global, factory=factory))
+
+    print({ 'width': width, 'depth': depth, 'magnetiization': magnetiization })
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
