71b87f8af0337c690a83e2853478b072944373f2
WrathfulSpatula <stranoj@gmail.com>
2025-05-16 20:12:37 -0400

    Fix typo

--- mitiq_qv.py
+++ mitiq_qv.py
@@ -25,126 +25,30 @@ from mitiq import zne
 from mitiq.zne.scaling.folding import fold_global
 from mitiq.zne.inference import LinearFactory
 
-    
-def factor_width(width):
-    col_len = math.floor(math.sqrt(width))
-    while (((width // col_len) * col_len) != width):
-        col_len -= 1
-    row_len = width // col_len
-    if col_len == 1:
-        raise Exception("ERROR: Can't simulate prime number width!")
-
-    return (row_len, col_len)
-
-
-def cx(sim, q1, q2):
-    sim.cx(q1, q2)
-
-
-def cy(sim, q1, q2):
-    sim.cy(q1, q2)
-
-
-def cz(sim, q1, q2):
-    sim.cz(q1, q2)
-
-
-def acx(sim, q1, q2):
-    sim.x(q1)
-    sim.cx(q1, q2)
-    sim.x(q1)
-
-
-def acy(sim, q1, q2):
-    sim.x(q1)
-    sim.cy(q1, q2)
-    sim.x(q1)
-
-
-def acz(sim, q1, q2):
-    sim.x(q1)
-    sim.cz(q1, q2)
-    sim.x(q1)
-
-
-def swap(sim, q1, q2):
-    sim.swap(q1, q2)
-
-
-def iswap(sim, q1, q2):
-    sim.iswap(q1, q2)
-
-
-def iiswap(sim, q1, q2):
-    sim.iswap(q1, q2)
-    sim.iswap(q1, q2)
-    sim.iswap(q1, q2)
-
-
-def pswap(sim, q1, q2):
-    sim.cz(q1, q2)
-    sim.swap(q1, q2)
-
-
-def mswap(sim, q1, q2):
-    sim.swap(q1, q2)
-    sim.cz(q1, q2)
-
-
-def nswap(sim, q1, q2):
-    sim.cz(q1, q2)
-    sim.swap(q1, q2)
-    sim.cz(q1, q2)
-
 
 def random_circuit(width, depth):
     # This is a "nearest-neighbor" coupler random circuit.
+    control = AerSimulator(method="statevector")
+    shots = 1 << (width + 2)
+
     lcv_range = range(width)
     all_bits = list(lcv_range)
-    
-    # Nearest-neighbor couplers:
-    gateSequence = [ 0, 3, 2, 1, 2, 1, 0, 3 ]
-    two_bit_gates = swap, pswap, mswap, nswap, iswap, iiswap, cx, cy, cz, acx, acy, acz
-    
-    row_len, col_len = factor_width(width)
-    
-    results = []
 
     circ = QuantumCircuit(width)
     for d in range(depth):
         # Single-qubit gates
         for i in lcv_range:
-            circ.h(i)
-            circ.rz(random.uniform(0, 2 * math.pi), i)
+            for _ in range(3):
+                circ.h(i)
+                circ.rz(random.uniform(0, 2 * math.pi), i)
 
-        # Nearest-neighbor couplers:
-        ############################
-        gate = gateSequence.pop(0)
-        gateSequence.append(gate)
-        for row in range(1, row_len, 2):
-            for col in range(col_len):
-                temp_row = row
-                temp_col = col
-                temp_row = temp_row + (1 if (gate & 2) else -1);
-                temp_col = temp_col + (1 if (gate & 1) else 0)
-
-                if temp_row < 0:
-                    temp_row = temp_row + row_len
-                if temp_col < 0:
-                    temp_col = temp_col + col_len
-                if temp_row >= row_len:
-                    temp_row = temp_row - row_len
-                if temp_col >= col_len:
-                    temp_col = temp_col - col_len
-
-                b1 = row * row_len + col
-                b2 = temp_row * row_len + temp_col
-
-                if (b1 >= width) or (b2 >= width):
-                    continue
-
-                g = random.choice(two_bit_gates)
-                g(circ, b1, b2)
+        # 2-qubit couplers
+        unused_bits = all_bits.copy()
+        random.shuffle(unused_bits)
+        while len(unused_bits) > 1:
+            c = unused_bits.pop()
+            t = unused_bits.pop()
+            circ.cx(c, t)
 
     return circ
 
