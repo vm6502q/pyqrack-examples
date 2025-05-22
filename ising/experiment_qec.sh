fe4f261529e4a5bd38dd3b4dc4533a64abfbf6a0
WrathfulSpatula <stranoj@gmail.com>
2025-05-22 09:43:18 -0400

    Reverse argument order, rename files

--- ising/experiment_qec.sh
+++ ising/experiment_qec.sh
@@ -3,4 +3,4 @@ export QRACK_QTENSORNETWORK_THRESHOLD_QB=-1
 # Suggested value for SDRP
 export QRACK_QUNIT_SEPARABILITY_THRESHOLD=0.02375
 
-for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25 26 27 28; do python3 ising_ace_validation.py 10 $w 2048; done
+for w in 4 6 8 9 10 12 14 15 16 18 20 21 22 24 25 26 27 28; do python3 ising_ace_validation.py $w 10 2048; done
