017c297ea8b51b2adaff04f1852068383e3f955b
WrathfulSpatula <stranoj@gmail.com>
2025-05-23 12:46:47 -0400

    2^16 shots default for TFIM

--- ising/ising.py
+++ ising/ising.py
@@ -80,7 +80,7 @@ def main():
     n_qubits = 56
     depth = 10
     reverse = False
-    shots = 8192
+    shots = 65536
     if len(sys.argv) > 1:
         n_qubits = int(sys.argv[1])
     if len(sys.argv) > 2:
