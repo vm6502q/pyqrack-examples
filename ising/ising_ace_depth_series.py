1760249072b45b71899be49c8a6f7db20f16bb57
WrathfulSpatula <stranoj@gmail.com>
2025-07-06 16:47:48 -0400

    Independent intercept

--- ising/ising_ace_depth_series.py
+++ ising/ising_ace_depth_series.py
@@ -108,7 +108,7 @@ def main():
     print("Devices: " + str(devices))
 
     n_rows, n_cols = factor_width(n_qubits, False)
-    bias_shots = int((1.795 / n_qubits - (depth - 1) / 200) * shots)
+    bias_shots = int((1.785 / n_qubits - (depth - 1) / 200) * shots)
     remainder_shots = shots - bias_shots
 
     # Quantinuum settings
