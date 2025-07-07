4190bc727870c4aa8e1c39262a34e3665dacd6fe
WrathfulSpatula <stranoj@gmail.com>
2025-07-07 04:31:25 -0400

    Single-parameter model

--- ising/ising_ace_validation.py
+++ ising/ising_ace_validation.py
@@ -201,11 +201,11 @@ def main():
 
     nq_2 = n_qubits * (n_qubits - 1)
     nq_3 = n_qubits * (n_qubits - 1) * (n_qubits - 2)
-    model = 1.75 - n_qubits * ((0.014 * (depth - 1)) ** 2)
-    bias_0_shots = int(shots * model / n_qubits)
-    bias_1_shots = int(shots * model / 2) // n_qubits
-    bias_2_shots = n_qubits * (int(shots * model / 4) // nq_2)
-    bias_3_shots = nq_2 * (int(shots * model / 8) // nq_3)
+    c_0 = 1.75
+    bias_0_shots = int(shots * c_0 / n_qubits)
+    bias_1_shots = int(shots * c_0 / 2) // n_qubits
+    bias_2_shots = n_qubits * (int(shots * c_0 / 4) // nq_2)
+    bias_3_shots = nq_2 * (int(shots * c_0 / 8) // nq_3)
     remainder_shots = shots - (bias_0_shots + bias_1_shots + bias_2_shots + bias_3_shots)
 
     qc = QuantumCircuit(n_qubits)
