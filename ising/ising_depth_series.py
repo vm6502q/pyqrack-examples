5835f5ff78051cb96034b085a34ff684aae6b9e6
WrathfulSpatula <stranoj@gmail.com>
2025-07-12 04:20:59 -0400

    Tune simple ACE

--- ising/ising_depth_series.py
+++ ising/ising_depth_series.py
@@ -148,8 +148,8 @@ def main():
             if d > 0:
                 experiment.run_qiskit_circuit(step)
 
-                t1 = 6.5
-                t2 = 2
+                t1 = 7.0
+                t2 = 0.5
                 t = d * dt
                 m = t / t1
                 model = 1 - 1 / (1 + m)
@@ -163,7 +163,7 @@ def main():
                     d_magnetization = 1 if J < 0 else -1
                     d_sqr_magnetization = 1
                 else:
-                    p = 2**arg - math.tanh(J / abs(h)) * math.log(1 + t / t2) / math.log(2)
+                    p = 2**arg + math.tanh(J / abs(h)) * math.log(1 + t / t2) / math.log(2)
                     factor = 2**p
                     n = 1 / (n_qubits * 2)
                     tot_n = 0
--- ising/model_fit.py
+++ ising/model_fit.py
@@ -165,8 +165,8 @@ def main():
     depth = 20
     hamming_n = 2048
     trials = 20
-    t1 = 6.5
-    t2 = 2
+    t1 = 7.0
+    t2 = 0.5
 
     print("t1: " + str(t1))
     print("t2: " + str(t2))
@@ -189,7 +189,7 @@ def main():
     # J, h, dt = -1.0, 1.0, 0.25
     # theta = -math.pi / 4
 
-    shots = 1 << (n_qubits + 2)
+    shots = max(65536, 1 << (n_qubits + 2))
     qubits = list(range(n_qubits))
 
     qc = QuantumCircuit(n_qubits)
@@ -225,7 +225,7 @@ def main():
                 if J > 0:
                     bias.reverse()
             else:
-                p = 2**arg - math.tanh(J / abs(h)) * math.log(1 + t / t2) / math.log(2)
+                p = 2**arg + math.tanh(J / abs(h)) * math.log(1 + t / t2) / math.log(2)
                 factor = 2**p
                 n = 1 / (n_qubits * 2)
                 tot_n = 0
