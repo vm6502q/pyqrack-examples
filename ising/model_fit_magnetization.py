876d9551918c77ad9815e862c9d6fe637d8a3b53
WrathfulSpatula <stranoj@gmail.com>
2025-07-16 16:27:38 -0400

    Better omega

--- ising/model_fit_magnetization.py
+++ ising/model_fit_magnetization.py
@@ -80,7 +80,7 @@ observed_data = np.array(
 with pm.Model() as model:
     # Priors
     t2 = pm.Uniform("t2", lower=0.125, upper=4)
-    omega = pm.Uniform("omega", lower=1.5, upper=2.5)
+    omega = pm.Uniform("omega", lower=2.4, upper=3.0)
 
     # Forward model
     mu = magnetization_model(depths, dt, n_qubits, J, h, t2, omega)
