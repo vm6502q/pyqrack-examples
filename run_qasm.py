9164a6c389af7a84b32c209a70a5b42c178615d4
WrathfulSpatula <stranoj@gmail.com>
2025-08-19 11:54:00 -0400

    1e6 shots

--- run_qasm.py
+++ run_qasm.py
@@ -1,5 +1,6 @@
 # Convert any circuit to a near-Clifford tableau
 
+import json
 import sys
 
 from collections import Counter
@@ -10,21 +11,27 @@ from qiskit import QuantumCircuit
 from qiskit.compiler import transpile
 
 
-def run_qasm(file_in):
+def run_qasm(file_in, file_out):
     qc = QuantumCircuit.from_qasm_file(file_in)
     # Near-Clifford basis set
     qc = transpile(qc, basis_gates=QrackSimulator.get_qiskit_basis_gates())
     sim = QrackSimulator(qc.num_qubits)
     sim.run_qiskit_circuit(qc, shots=0)
-    print(Counter(sim.measure_shots(list(range(qc.num_qubits)), 8192)))
+    shots = dict(Counter(sim.measure_shots(list(range(qc.num_qubits)), 1048576)))
+    with open(file_out, "w") as f:
+        json.dump(shots, f)
+    print(shots)
 
 
 def main():
     file_in = "qft.qasm"
+    file_out = "out.json"
     if len(sys.argv) > 1:
         file_in = str(sys.argv[1])
+    if len(sys.argv) > 2:
+        file_out = str(sys.argv[2])
 
-    run_qasm(file_in)
+    run_qasm(file_in, file_out)
 
     return 0
 
