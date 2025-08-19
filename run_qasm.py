94b663f565895986ccee6829afe17dfdd1570604
WrathfulSpatula <stranoj@gmail.com>
2025-08-19 15:07:21 -0400

    run_qasm.py

--- run_qasm.py
+++ run_qasm.py
@@ -10,17 +10,51 @@ from pyqrack import QrackSimulator
 from qiskit import QuantumCircuit
 from qiskit.compiler import transpile
 
+def int_to_bitstring(integer, length, reverse):
+    return (bin(integer)[2:].zfill(length))[::-1] if reverse else (bin(integer)[2:].zfill(length))
+
 
 def run_qasm(file_in, file_out):
+    shot_count = 1024
+    # shot_count = 8388608
     qc = QuantumCircuit.from_qasm_file(file_in)
-    # Near-Clifford basis set
-    qc = transpile(qc, basis_gates=QrackSimulator.get_qiskit_basis_gates())
-    sim = QrackSimulator(qc.num_qubits)
+    basis_gates = QrackSimulator.get_qiskit_basis_gates()
+    # basis_gates = [
+    #     "rz",
+    #     "h",
+    #     "x",
+    #     "y",
+    #     "z",
+    #     "sx",
+    #     "sxdg",
+    #     "s",
+    #     "sdg",
+    #     "t",
+    #     "tdg",
+    #     "cx",
+    #     "cy",
+    #     "cz",
+    #     "swap",
+    #     "iswap",
+    # ]
+    qc = transpile(qc, basis_gates=basis_gates)
+    sim = QrackSimulator(qc.num_qubits, isTensorNetwork=False)
+    # sim = QrackSimulator(qc.num_qubits, isTensorNetwork=False, isSchmidtDecompose=False, isStabilizerHybrid=True)
+    # sim.set_ncrp(2.0)
     sim.run_qiskit_circuit(qc, shots=0)
-    shots = dict(Counter(sim.measure_shots(list(range(qc.num_qubits)), 1048576)))
+    print("Fidelity estimate: " + str(sim.get_unitary_fidelity()))
+    shots = dict(Counter(sim.measure_shots(list(range(qc.num_qubits)), shot_count)))
     with open(file_out, "w") as f:
         json.dump(shots, f)
-    print(shots)
+    # sim.m_all()
+    # print("Fidelity estimate: " + str(sim.get_unitary_fidelity()))
+    max_key = max(shots, key=shots.get)
+    print(f"Total shots: {shot_count}")
+    print(f"Peak counts: {(max_key, shots[max_key])}")
+    rtl = int_to_bitstring(max_key, qc.num_qubits, False)
+    ltr = int_to_bitstring(max_key, qc.num_qubits, True)
+    print(f"Right-to-left, least-to-most significant: {rtl}")
+    print(f"Left-to-right, least-to-most significant: {ltr}")
 
 
 def main():
