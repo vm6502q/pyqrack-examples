afe1dc624e4e5b60c0ccfa94fd8acac00310dbbf
WrathfulSpatula <stranoj@gmail.com>
2025-08-20 20:47:11 -0400

    shor_highest_prob.py

--- algorithms/shor_highest_prob.py
+++ algorithms/shor_highest_prob.py
@@ -25,7 +25,7 @@ def phase_root_n(sim, n, q):
     sim.mtrx([1, 0, 0, -(1 ** (1.0 / (1 << (n - 1))))], q)
 
 
-def shor(to_factor, shots):
+def shor(to_factor):
     # Based on https://arxiv.org/abs/quant-ph/0205095
     start = time.perf_counter()
     base = random.randrange(2, to_factor)
@@ -58,45 +58,38 @@ def shor(to_factor, shots):
 
     sim.iqft(qo)
 
-    m_res = sim.measure_shots(qo, shots)
+    y = sim.highest_prob_perm() & ((1 << len(qo)) - 1)
 
-    r = 0
-    isSuccess = False
-    for y, _ in m_res.items():
-        r = Fraction(y).limit_denominator(to_factor - 1).denominator
+    r = Fraction(y).limit_denominator(to_factor - 1).denominator
 
-        # try to determine the factors
-        if r % 2 != 0:
-            r *= 2
-        apowrhalf = pow(base, r >> 1, to_factor)
-        f1 = gcd(apowrhalf + 1, to_factor)
-        f2 = gcd(apowrhalf - 1, to_factor)
-        fmul = f1 * f2
-        if (
-            (not fmul == to_factor)
-            and f1 * f2 > 1
-            and (to_factor // fmul) * fmul == to_factor
-        ):
-            f1, f2 = f1 * f2, to_factor // (f1 * f2)
-        if f1 * f2 == to_factor and f1 > 1 and f2 > 1:
-            print("Factors found : {} * {} = {}".format(f1, f2, to_factor))
-            isSuccess = True
-            break
-
-    if not isSuccess:
+    # try to determine the factors
+    if r % 2 != 0:
+        r *= 2
+    apowrhalf = pow(base, r >> 1, to_factor)
+    f1 = gcd(apowrhalf + 1, to_factor)
+    f2 = gcd(apowrhalf - 1, to_factor)
+    fmul = f1 * f2
+    if (
+        (not fmul == to_factor)
+        and f1 * f2 > 1
+        and (to_factor // fmul) * fmul == to_factor
+    ):
+        f1, f2 = f1 * f2, to_factor // (f1 * f2)
+    if f1 * f2 == to_factor and f1 > 1 and f2 > 1:
+        print("Factors found : {} * {} = {}".format(f1, f2, to_factor))
+    else:
         print("Failed.")
 
     print("Time: " + str(time.perf_counter() - start) + "seconds")
 
 
 def main():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         raise RuntimeError("Usage: python3 qbdd_shor.py [to_factor]")
 
     to_factor = int(sys.argv[1])
-    shots = int(sys.argv[2])
 
-    shor(to_factor, shots)
+    shor(to_factor)
 
     return 0
 
