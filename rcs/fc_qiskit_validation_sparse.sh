5bcd13e21e27788171c227437f265bcb81b22974
WrathfulSpatula <stranoj@gmail.com>
2025-11-13 23:36:03 -0500

    Debug

--- rcs/fc_qiskit_validation_sparse.sh
+++ rcs/fc_qiskit_validation_sparse.sh
@@ -1,1 +1,1 @@
-for n in {2..30}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$( echo "scale=64; 1/(16*2^16)" | bc ) python3 fc_qiskit_validation_sparse.py $n $n; done
+for n in {2..30}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$( echo "scale=64; 1/($n*2^($n/2))" | bc ) python3 fc_qiskit_validation_sparse.py $n $n; done
