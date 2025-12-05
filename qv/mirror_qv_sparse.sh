24a3220c21863e0b6ef12cf8efda2b380b7e0dad
Daniel Strano <stranoj@gmail.com>
2025-12-04 22:51:05 -0500

    Update mirror_qv_sparse.sh

--- qv/mirror_qv_sparse.sh
+++ qv/mirror_qv_sparse.sh
@@ -1,1 +1,1 @@
-for n in {2..50}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(( bc -l << "2^(-$n)/$n" )) python3 mirror_qv_sparse.py $n $n; done
+for n in {2..50}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(python3 -c "import math; print(1/(($n-1)*math.sqrt(2**$n)))") python3 mirror_qv_sparse.py $n $n; done
