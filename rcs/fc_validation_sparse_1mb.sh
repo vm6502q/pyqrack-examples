d772c30f7df235cca7524f5b17fe5381ed5a5e83
WrathfulSpatula <stranoj@gmail.com>
2025-12-05 21:39:54 -0500

    Revert "Optimize"
    This reverts commit 899fa87786a61f27fcc4f906ff5e74db6e21e508.

--- rcs/fc_validation_sparse_1mb.sh
+++
@@ -1,3 +0,0 @@
-export QRACK_SPARSE_MAX_ALLOC_MB=1
-for n in {2..16}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(python3 -c "import math; print(1/(($n-1)*math.sqrt(2**$n)))") python3 fc_validation_sparse.py $n $n; done
-for n in {17..30}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(python3 -c "import math; print(1/(($n-1)*math.sqrt(2**$n)))") python3 fc_validation_sparse.py $n 11; done
