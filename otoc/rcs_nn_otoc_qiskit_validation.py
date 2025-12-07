c490abf6d4f3910f5aa8084036d1a683b2812e18
WrathfulSpatula <stranoj@gmail.com>
2025-12-07 15:12:59 -0500

    is_sparse option for OTOC

--- otoc/rcs_nn_otoc_qiskit_validation.py
+++ otoc/rcs_nn_otoc_qiskit_validation.py
@@ -58,7 +58,7 @@ def acz(sim, q1, q2):
     sim.x(q1)
 
 
-def bench_qrack(width, depth, cycles):
+def bench_qrack(width, depth, cycles, is_sparse):
     # This is a "nearest-neighbor" coupler random circuit.
 
     lcv_range = range(width)
@@ -127,7 +127,7 @@ def bench_qrack(width, depth, cycles):
         otoc &= rcs.inverse()
 
 
-    experiment = QrackSimulator(width, isTensorNetwork=False)
+    experiment = QrackSimulator(width, isTensorNetwork=False, isSparse=is_sparse, isOpenCL=not is_sparse)
     experiment.run_qiskit_circuit(otoc)
 
     otoc_aer = otoc.copy()
@@ -196,15 +196,18 @@ def calc_stats(ideal_probs, counts, depth, shots):
 def main():
     if len(sys.argv) < 4:
         raise RuntimeError(
-            "Usage: python3 fc_qiskit_validation.py [width] [depth] [cycles]"
+            "Usage: python3 fc_qiskit_validation.py [width] [depth] [cycles] [is_sparse]"
         )
 
     width = int(sys.argv[1])
     depth = int(sys.argv[2])
     cycles = int(sys.argv[3])
+    is_sparse = False
+    if len(sys.argv) > 4:
+        is_sparse = sys.argv not in ['False', '0']
 
     # Run the benchmarks
-    print(bench_qrack(width, depth, cycles))
+    print(bench_qrack(width, depth, cycles, is_sparse))
 
     return 0
 
