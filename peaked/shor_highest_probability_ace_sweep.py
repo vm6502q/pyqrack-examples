92b287cb4843d6670ed9cfa51aaa98c7ec8092c0
WrathfulSpatula <stranoj@gmail.com>
2025-12-15 18:43:12 -0500

    For stability

--- peaked/shor_highest_probability_ace_sweep.py
+++ peaked/shor_highest_probability_ace_sweep.py
@@ -45,7 +45,7 @@ def shor(to_factor, is_sparse):
 
         qubitCount = math.ceil(math.log2(to_factor))
         sim = QrackSimulator(
-            (qubitCount << 1) + 1, isSparse=is_sparse, isOpenCL=not is_sparse
+            (qubitCount << 1) + 1, isTensorNetwork=False, isSparse=is_sparse, isOpenCL=not is_sparse
         )
         qo = [i for i in range(qubitCount)]
         qa = [(i + qubitCount) for i in range(qubitCount)]
