2a363c23ce84315052699dd4ab6993c1525fb5f8
WrathfulSpatula <stranoj@gmail.com>
2026-02-18 15:05:28 -0500

    (Guessing)

--- fermi-hubbard/fermi_hubbard_ace_depth_series.py
+++ fermi-hubbard/fermi_hubbard_ace_depth_series.py
@@ -152,7 +152,7 @@ def main():
     if len(sys.argv) > 6:
         t2 = float(sys.argv[6])
     else:
-        t2 = 2.0
+        t2 = 3.0
     if len(sys.argv) > 7:
         shots = int(sys.argv[7])
     else:
@@ -205,7 +205,7 @@ def main():
 
         # The magnetization components are weighted by (n+1) symmetric "bias" terms over possible Hamming weights.
         bias_z = get_tfim_hamming_distribution(J=J, h=h, z=z, theta=theta, t=t_h, n_qubits=n_qubits)
-        bias_x = get_tfim_hamming_distribution(J=-h, h=-J, z=z, theta=theta + np.pi / 2, t=t_h, n_qubits=n_qubits)
+        bias_x = get_tfim_hamming_distribution(J=h, h=J, z=z, theta=theta + np.pi / 2, t=t_h, n_qubits=n_qubits)
         bias = [(z + x) / 2 for z, x in zip(bias_z, bias_x)]
 
         magnetization, sqr_magnetization = 0, 0
