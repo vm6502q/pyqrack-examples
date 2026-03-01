6eae4f3c4b6afac2408815666e57937f3798e322
WrathfulSpatula <stranoj@gmail.com>
2026-02-28 21:53:00 -0500

    Debug rounding

--- sparse/fc_qrack_validation_sparse.sh
+++ sparse/fc_qrack_validation_sparse.sh
@@ -1,1 +1,1 @@
-for n in {2..30}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(python3 -c "print(1/(($n-1)*(2**$n)))") python3 fc_qrack_validation_sparse.py $n $n; done
+for n in {2..30}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(python3 -c "print(max(1.7763568394002505e-15,1.0/(($n-1)*(2**$n))))") python3 fc_qrack_validation_sparse.py $n $n; done
--- sparse/rcs_nn_qiskit_validation_sparse.sh
+++ sparse/rcs_nn_qiskit_validation_sparse.sh
@@ -1,1 +1,1 @@
-for n in {4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(python3 -c "print(1/(4*(2**$n)))") python3 rcs_nn_qiskit_validation_sparse.py $n $n; done
+for n in {4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30}; do QRACK_SPARSE_TRUNCATION_THRESHOLD=$(python3 -c "print(max(1.7763568394002505e-15,1.0/(4*(2**$n))))") python3 rcs_nn_qiskit_validation_sparse.py $n $n; done
