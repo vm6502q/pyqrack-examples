9549cc245dde1059c8a370ad8ac3b0056759a344
WrathfulSpatula <stranoj@gmail.com>
2026-04-02 13:04:07 -0400

    Tunable p

--- rcs/turboquant/fc_tq_qiskit_validation.py
+++ rcs/turboquant/fc_tq_qiskit_validation.py
@@ -17,7 +17,7 @@ from qiskit_aer.backends import AerSimulator
 from qiskit.quantum_info import Statevector
 
 
-def bench_qrack(width, depth, trials, sdrp):
+def bench_qrack(width, depth, p, sdrp):
     # This is a "nearest-neighbor" coupler random circuit.
     control = AerSimulator(method="statevector")
     shots = 1 << (width + 2)
@@ -25,56 +25,40 @@ def bench_qrack(width, depth, trials, sdrp):
     lcv_range = range(width)
     all_bits = list(lcv_range)
 
-    results = []
+    circ = QuantumCircuit(width)
+    for d in range(depth):
+        # Single-qubit gates
+        for i in lcv_range:
+            th = random.uniform(0, 2 * math.pi)
+            ph = random.uniform(0, 2 * math.pi)
+            lm = random.uniform(0, 2 * math.pi)
+            circ.u(th, ph, lm, i)
 
-    for trial in range(trials):
-        circ = QuantumCircuit(width)
-        for d in range(depth):
-            # Single-qubit gates
-            for i in lcv_range:
-                th = random.uniform(0, 2 * math.pi)
-                ph = random.uniform(0, 2 * math.pi)
-                lm = random.uniform(0, 2 * math.pi)
-                circ.u(th, ph, lm, i)
+        # 2-qubit couplers
+        unused_bits = all_bits.copy()
+        random.shuffle(unused_bits)
+        while len(unused_bits) > 1:
+            c = unused_bits.pop()
+            t = unused_bits.pop()
+            circ.cx(c, t)
 
-            # 2-qubit couplers
-            unused_bits = all_bits.copy()
-            random.shuffle(unused_bits)
-            while len(unused_bits) > 1:
-                c = unused_bits.pop()
-                t = unused_bits.pop()
-                circ.cx(c, t)
+        experiment = QrackSimulator(width)
+        if sdrp > 0:
+            experiment.set_sdrp(sdrp)
+        experiment.run_qiskit_circuit(circ)
 
-            experiment = QrackSimulator(width)
-            if sdrp > 0:
-                experiment.set_sdrp(sdrp)
-            experiment.run_qiskit_circuit(circ)
+        # The point is to test whether XEB survives with a TurboQuant-based compression approach
+        experiment.lossy_out_to_file("fc.svtq", p=p)
+        experiment.lossy_in_from_file("fc.svtq")
 
-            # The point is to test whether XEB survives with a TurboQuant-based compression approach
-            experiment.lossy_out_to_file("fc.svtq", p=6)
-            experiment.lossy_in_from_file("fc.svtq")
+        circ_aer = circ.copy()
+        circ_aer.save_statevector()
+        job = control.run(circ_aer)
 
-            circ_aer = circ.copy()
-            circ_aer.save_statevector()
-            job = control.run(circ_aer)
+        experiment_counts = dict(Counter(experiment.measure_shots(all_bits, shots)))
+        control_probs = Statevector(job.result().get_statevector()).probabilities()
 
-            experiment_counts = dict(Counter(experiment.measure_shots(all_bits, shots)))
-            control_probs = Statevector(job.result().get_statevector()).probabilities()
-
-            stats = calc_stats(control_probs, experiment_counts, d + 1, shots)
-
-            if trial == 0:
-                results.append(stats)
-            else:
-                results[d]["xeb"] += stats["xeb"]
-                results[d]["hog_prob"] += stats["hog_prob"]
-                results[d]["p-value"] *= stats["p-value"]
-
-            if trial == (trials - 1):
-                results[d]["xeb"] /= trials
-                results[d]["hog_prob"] /= trials
-                results[d]["p-value"] = results[d]["p-value"] ** (1 / trials)
-                print(results[d])
+        print(calc_stats(control_probs, experiment_counts, d + 1, shots))
 
 
 def calc_stats(ideal_probs, counts, depth, shots):
@@ -118,20 +102,20 @@ def calc_stats(ideal_probs, counts, depth, shots):
 def main():
     if len(sys.argv) < 3:
         raise RuntimeError(
-            "Usage: python3 fc_qiskit_validation.py [width] [depth] [trials]"
+            "Usage: python3 fc_qiskit_validation.py [width] [depth] [compression block size power] [sdrp]"
         )
 
     width = int(sys.argv[1])
     depth = int(sys.argv[2])
-    trials = 1
+    p = 6
     sdrp = 0
     if len(sys.argv) > 3:
-        trials = int(sys.argv[3])
+        p = int(sys.argv[3])
     if len(sys.argv) > 4:
         sdrp = float(sys.argv[4])
 
     # Run the benchmarks
-    bench_qrack(width, depth, trials, sdrp)
+    bench_qrack(width, depth, p, sdrp)
 
     return 0
 
