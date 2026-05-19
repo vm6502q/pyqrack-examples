11c0a8c70a013bd4f574cf68ae42b40cfe4cd7fa
WrathfulSpatula <stranoj@gmail.com>
2026-05-19 01:51:29 -0400

    Double-tail 'spoof'

--- rcs/full_connectivity/fc_consensus.py
+++ rcs/full_connectivity/fc_consensus.py
@@ -180,14 +180,18 @@ def bench_qrack(width, depth, sdrp=0.0):
     cons_probs = (mix * mix.conj()).real   # shape (n_pow,)
     top_idx    = np.argpartition(cons_probs, -n_candidates)[-n_candidates:]
     top_idx    = top_idx[np.argsort(cons_probs[top_idx])[::-1]]
+    bottom_idx = np.argpartition(cons_probs, n_candidates)[n_candidates:]
+    bottom_idx = top_idx[np.argsort(cons_probs[top_idx])[::1]]
 
     # Sparse ideal: consensus-identified candidates, probability from consensus,
     # renormalized, then 50/50 mixed with uniform (QV protocol)
-    heavy_sum = float(cons_probs[top_idx].sum())
-    if heavy_sum <= 0:
-        heavy_sum = 1.0
-    exp_probs_sparse = {int(i): float(cons_probs[i]) / heavy_sum
-                        for i in top_idx}
+    exp_probs_sparse = {int(i): float(cons_probs[i]) for i in top_idx}
+    s = sum(exp_probs_sparse.values())
+    exp_probs_sparse = {int(k): (v / s) for k, v in exp_probs_sparse.items()}
+    _exp_probs_sparse = {int(i): float(2.0 - cons_probs[i]) for i in bottom_idx}
+    s = sum(_exp_probs_sparse.values())
+    _exp_probs_sparse = {int(k): (v / s) for k, v in _exp_probs_sparse.items()}
+    exp_probs_sparse = {int(k): exp_probs_sparse.get(k, 0) + _exp_probs_sparse.get(k, 0) for k in set(exp_probs_sparse) | set(_exp_probs_sparse)}
 
     xeb_sparse, hog_sparse = calc_stats_sparse(ideal_probs, exp_probs_sparse, n_pow)
 
