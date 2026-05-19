407d1229556d6df69a0c6931509976887ff4bce4
WrathfulSpatula <stranoj@gmail.com>
2026-05-19 01:57:09 -0400

    White space

--- rcs/full_connectivity/fc_consensus.py
+++ rcs/full_connectivity/fc_consensus.py
@@ -188,9 +188,11 @@ def bench_qrack(width, depth, sdrp=0.0):
     exp_probs_sparse = {int(i): float(cons_probs[i]) for i in top_idx}
     s = sum(exp_probs_sparse.values())
     exp_probs_sparse = {int(k): (v / s) for k, v in exp_probs_sparse.items()}
+
     _exp_probs_sparse = {int(i): float(2.0 - cons_probs[i]) for i in bottom_idx}
     s = sum(_exp_probs_sparse.values())
     _exp_probs_sparse = {int(k): (2.0 - (v / s)) for k, v in _exp_probs_sparse.items()}
+
     exp_probs_sparse = {int(k): exp_probs_sparse.get(k, 0) + _exp_probs_sparse.get(k, 0) for k in set(exp_probs_sparse) | set(_exp_probs_sparse)}
 
     xeb_sparse, hog_sparse = calc_stats_sparse(ideal_probs, exp_probs_sparse, n_pow)
