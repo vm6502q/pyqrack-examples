99066a6e6a34b0c2f609457f0bf86dec76a67d10
WrathfulSpatula <stranoj@gmail.com>
2026-08-09 09:21:00 -0400

    Debug NC

--- nc/Hybrid_NC_Method/qs_rcs_nn_qrack_validation.py
+++ nc/Hybrid_NC_Method/qs_rcs_nn_qrack_validation.py
@@ -282,9 +282,9 @@ def calc_stats(ideal_probs, probs, shots, depth, magic):
     experiment = [0] * n_pow
     probs_heavy, probs_light = probs
     probs_light = {k: (1.0 - v) for k, v in probs_light.items()} if len(probs_light) else {}
-    n_light = 1.0 / (u_u * ((n_pow - len(probs_light)) + sum((1.0 - v) for v in probs_light.values())))
+    n_light = 1.0 / (u_u * (n_pow - len(probs_light)) + sum(u_u * (1.0 - v) for v in probs_light.values()))
     for i in range(n_pow):
-        exp = 0.5 * probs_heavy.get(i, 0)  +  0.5 * u_u * n_light * (1.0 - probs_light.get(i, 0))
+        exp = 0.5 * probs_heavy.get(i, 0)  +  0.5 * n_light * u_u * (1.0 - probs_light.get(i, 0))
         ideal = ideal_probs[i]
         count = exp * shots
 
