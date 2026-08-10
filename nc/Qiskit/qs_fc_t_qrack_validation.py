ade2bda22123effadf84f751ea155c14040b2b3f
WrathfulSpatula <stranoj@gmail.com>
2026-08-10 13:59:04 -0400

    T variant

--- nc/Qiskit/qs_fc_t_qrack_validation.py
+++ nc/Qiskit/qs_fc_t_qrack_validation.py
@@ -12,24 +12,14 @@ from qiskit import QuantumCircuit, transpile
 from qiskit.providers.qrack import QStabilizerQasmSimulator
 
 
-def bench_qrack(n_qubits, magic, shots):
+def bench_qrack(n_qubits, shots):
     # This is a "fully-connected" coupler random circuit.
     lcv_range = range(n_qubits)
     all_bits = list(lcv_range)
     mean = 1.0 / (1 << n_qubits)
 
-    rz_count = magic
-    rz_opportunities = n_qubits * n_qubits * 2
-    rz_positions = []
-    while len(rz_positions) < rz_count:
-        rz_position = random.randint(0, rz_opportunities - 1)
-        if rz_position in rz_positions:
-            continue
-        rz_positions.append(rz_position)
-
     qc = QuantumCircuit(n_qubits)
     control = QrackSimulator(n_qubits)
-    aux = QrackSimulator(n_qubits, is_near_clifford_tableau_writer=True)
     gate_count = 0
     magic_count = 0
     for d in range(n_qubits):
@@ -39,21 +29,16 @@ def bench_qrack(n_qubits, magic, shots):
             for _ in range(2):
                 qc.h(i)
                 control.h(i)
-                aux.h(i)
-                s_count = random.randint(0, 3)
+                s_count = random.randint(0, 7)
                 if s_count & 1:
                     qc.z(i)
                     control.z(i)
-                    aux.z(i)
                 if s_count & 2:
                     qc.s(i)
                     control.s(i)
-                    aux.s(i)
-                if gate_count in rz_positions:
-                    angle = random.uniform(0, math.pi / 2)
-                    qc.rz(angle, i)
-                    control.r(Pauli.PauliZ, angle, i)
-                    aux.r(Pauli.PauliZ, angle, i)
+                if s_count & 4:
+                    qc.t(i)
+                    control.t(i)
                     magic_count += 1
                 gate_count = gate_count + 1
 
@@ -65,7 +50,6 @@ def bench_qrack(n_qubits, magic, shots):
             t = unused_bits.pop()
             qc.cx(c, t)
             control.mcx([c], t)
-            aux.mcx([c], t)
         
         experiment = QStabilizerQasmSimulator(n_qubits=n_qubits)
         aer_qc = transpile(qc, backend=experiment, optimization_level=1)
@@ -108,19 +92,14 @@ def calc_stats(ideal_probs, counts, shots, depth, magic, n):
 
 def main():
     n_qubits = 16
-    magic = 17
     shots = 1024
     if len(sys.argv) > 1:
         n_qubits = int(sys.argv[1])
     if len(sys.argv) > 2:
-        magic = int(sys.argv[2])
-    else:
-        magic = n_qubits + 1
-    if len(sys.argv) > 3:
-        shots = int(sys.argv[3])
+        shots = int(sys.argv[2])
 
     # Run the benchmarks
-    bench_qrack(n_qubits, magic, shots)
+    bench_qrack(n_qubits, shots)
 
     return 0
 
