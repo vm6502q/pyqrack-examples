c7bf36ada89f6c87f92ed042d0a130faa448a135
WrathfulSpatula <stranoj@gmail.com>
2026-09-12 07:48:38 -0400

    Oops

--- qec/ccnot.py
+++ qec/ccnot.py
@@ -6,20 +6,7 @@ import sys
 
 from pyqrack import QrackAceBackend
 
-
-def main():
-    experiment = QrackAceBackend(15, long_range_columns=2)
-
-    # Experiment has a cleaved-QEC code ACE boundary.
-    experiment.h(0)
-    experiment.h(1)
-
-    # Error-detection
-    experiment.cx(0, 6)
-    experiment.cx(1, 6)
-    experiment.cx(1, 7)
-    experiment.cx(2, 11)
-
+def ccnot(experiment):
     experiment.h(2)
     experiment.cx(1, 2)
     experiment.adjt(2)
@@ -35,6 +22,51 @@ def main():
     experiment.t(0)
     experiment.adjt(1)
     experiment.cx(0, 1)
+
+
+def output(experiment):
+    shots = 1024
+    counts = experiment.measure_shots([0, 1, 2], shots)
+
+    one = 0
+    uncorrelated = 0
+    for count in counts:
+        if (count == 3) or (count == 4):
+            uncorrelated += 1
+        elif count == 7:
+            one += 1
+    correlated = shots - uncorrelated
+
+    print("Correlation: " + str(correlated / shots))
+    print("[1, 1, 1] count: " + (str(one / correlated) if correlated else "N/A"))
+
+
+def main():
+    experiment = QrackAceBackend(15, long_range_columns=2)
+
+    # Experiment has a cleaved-QEC code ACE boundary.
+    experiment.h(0)
+    experiment.h(1)
+
+    ccnot(experiment)
+
+    print("Uncorrected:")
+    output(experiment)
+    print()
+
+    experiment = QrackAceBackend(15, long_range_columns=2)
+
+    # Experiment has a cleaved-QEC code ACE boundary.
+    experiment.h(0)
+    experiment.h(1)
+
+    # Error-detection
+    experiment.cx(0, 6)
+    experiment.cx(1, 6)
+    experiment.cx(1, 7)
+    experiment.cx(2, 11)
+
+    ccnot(experiment)
 
     # Syndrome
     experiment.cx(2, 11)
@@ -52,21 +84,8 @@ def main():
     experiment.force_m(6, False)
     experiment.force_m(7, False)
 
-    # Any correlation above 0.5 is entanglement non-locality.
-    shots = 1024
-    counts = experiment.measure_shots([0, 1, 2], shots)
-
-    one = 0
-    uncorrelated = 0
-    for count in counts:
-        if count == 3:
-            uncorrelated += 1
-        elif count == 7:
-            one += 1
-    correlated = shots - uncorrelated
-
-    print("Correlation: " + str(correlated / shots))
-    print("[1, 1, 1] count: " + (str(one / correlated) if correlated else "N/A"))
+    print("Corrected:")
+    output(experiment)
 
 
 if __name__ == "__main__":
