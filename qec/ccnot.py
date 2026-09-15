0d43b9509816ea06a7acfef2627d209562f9f7d1
WrathfulSpatula <stranoj@gmail.com>
2026-09-14 22:31:14 -0400

    Print all CCNOT outcomes

--- qec/ccnot.py
+++ qec/ccnot.py
@@ -29,27 +29,53 @@ def output(experiment, c1, c2, t):
     shots = 1024
     counts = experiment.measure_shots([c1, c2, t], shots)
 
+    correlated = 0
     uncorrelated = 0
+    zero = 0
+    one = 0
+    two = 0
     three = 0
     four = 0
+    five = 0
+    six = 0
     seven = 0
     for count in counts:
-        if count == 3:
+        if count == 0:
+            zero += 1
+            correlated += 1
+        elif count == 1:
+            one += 1
+            correlated += 1
+        elif count == 2:
+            two += 1
+            correlated += 1
+        elif count == 3:
             three += 1
             uncorrelated += 1
         elif count == 4:
             four += 1
             uncorrelated += 1
+        elif count == 5:
+            five += 1
+            uncorrelated += 1
+        elif count == 6:
+            six += 1
+            uncorrelated += 1
         elif count == 7:
             seven += 1
-    correlated = shots - uncorrelated
+            correlated += 1
 
     print("Correlation: " + str(correlated / shots))
     if uncorrelated:
         print("[1, 1, 0] frequency (of uncorrelated): " + str(three / uncorrelated))
         print("[0, 0, 1] frequency (of uncorrelated): " + str(four / uncorrelated))
-    print("[1, 1, 1] frequency (of correlated): " + (str(seven / correlated) if correlated else "N/A"))
-    print("[0, 0, 0] frequency (of correlated): " + (str(1 - (seven / correlated)) if correlated else "N/A"))
+        print("[1, 0, 1] frequency (of uncorrelated): " + str(five / uncorrelated))
+        print("[0, 1, 1] frequency (of uncorrelated): " + str(six / uncorrelated))
+    if correlated:
+        print("[1, 0, 0] frequency (of correlated): " + (str(one / correlated) if correlated else "N/A"))
+        print("[0, 1, 0] frequency (of correlated): " + (str(two / correlated) if correlated else "N/A"))
+        print("[1, 1, 1] frequency (of correlated): " + (str(seven / correlated) if correlated else "N/A"))
+        print("[0, 0, 0] frequency (of correlated): " + (str(zero / correlated) if correlated else "N/A"))
 
 
 def main():
@@ -66,35 +92,6 @@ def main():
     print("Uncorrected:")
     output(experiment, c1, c2, t)
     print()
-
-    experiment = QrackAceBackend(10, long_range_columns=2)
-
-    # Experiment has a cleaved-QEC code ACE boundary.
-    experiment.h(c1)
-    experiment.h(c2)
-
-    # Error-detection
-    experiment.cx(t, 5)
-    experiment.cx(c1, 5)
-    experiment.cx(c2, 5)
-    experiment.cx(t, 6)
-    experiment.cx(c2, 6)
-    experiment.cx(c1, 6)
-
-    ccnot(experiment, c1, c2, t)
-
-    # Syndrome
-    experiment.cx(t, 5)
-    experiment.cx(c2, 5)
-    experiment.cx(t, 6)
-    experiment.cx(c1, 6)
-
-    # Post-selection
-    experiment.force_m(5, False)
-    experiment.force_m(6, False)
-
-    print("Corrected:")
-    output(experiment, c1, c2, t)
 
 
 if __name__ == "__main__":
