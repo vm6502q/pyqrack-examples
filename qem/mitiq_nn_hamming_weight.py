45c14e5e6c3f87704bbe972c27a7378dbdd06509
WrathfulSpatula <stranoj@gmail.com>
2026-09-16 23:22:51 -0400

    Power-of-2 scale count, stay short of 2x max scale, Richardson, and to Hell with it all

--- qem/mitiq_nn_hamming_weight.py
+++ qem/mitiq_nn_hamming_weight.py
@@ -20,7 +20,7 @@ from qiskit.providers.qrack import AceQasmSimulator
 
 from mitiq import zne
 from mitiq.zne.scaling.folding import fold_global
-from mitiq.zne.inference import LinearFactory
+from mitiq.zne.inference import RichardsonFactory
 
 
 def factor_width(width):
@@ -207,9 +207,9 @@ def main():
 
     raw = width * expit(execute(qc, width, shots))
 
-    scale_count = 9
+    scale_count = 8
     max_scale = 2
-    factory = LinearFactory(
+    factory = RichardsonFactory(
         scale_factors=[
             (1 + (max_scale - 1) * x / scale_count) for x in range(0, scale_count)
         ]
